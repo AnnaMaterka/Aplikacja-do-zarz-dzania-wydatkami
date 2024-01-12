20e6a9c9b1ca754ec9e22d9e421a663433857c60
JakubPacz <japaczek@student.agh.edu.pl>
2024-01-12 14:40:33 +0100

    Wydatek

---
+++ Aplikacja_do_zarzadzania_wydatkami/Aplikacja_do_zarzadzania_wydatkami/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja_do_zarzadzania_wydatkami
+{
+    public abstract class Wydatek
+    {
+        private decimal kwota;
+        private DateTime data;
+        private string kategoria;
+
+        public decimal Kwota { get => kwota; set => kwota = value; }
+        public DateTime Data { get => data; set => data = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+    }
+}
