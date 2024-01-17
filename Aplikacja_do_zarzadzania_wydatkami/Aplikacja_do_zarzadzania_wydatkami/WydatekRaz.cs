177e5c04f3b19458a008ab08c0909abf948d3391
dmazurczak@student.agh.edu.pl <dmazurczak@student.agh.edu.pl>
2024-01-17 22:07:29 +0100

    wplywy

--- Aplikacja_do_zarzadzania_wydatkami/Aplikacja_do_zarzadzania_wydatkami/WydatekRaz.cs
+++ Aplikacja_do_zarzadzania_wydatkami/Aplikacja_do_zarzadzania_wydatkami/WydatekRaz.cs
@@ -6,17 +6,27 @@ using System.Threading.Tasks;
 
 namespace Aplikacja_do_zarzadzania_wydatkami
 {
-    public enum KategoriaWydatku { Spożywcze, Ubrania, Buty, Opłaty, Rozrywka, Elektronika, Uroda, DoDomu, Inne }
+    //public enum KategoriaWydatku { Spożywcze, Ubrania, Buty, Opłaty, Rozrywka, Elektronika, Uroda, DoDomu, Inne }
     public class WydatekRaz : Wydatek
     {
         private decimal kwota;
         private DateTime data;
-        private KategoriaWydatku kategoria;
+        //private KategoriaWydatku kategoria;
+        private string kategoria;
 
-        public WydatekRaz(decimal kwota, DateTime data, KategoriaWydatku kategoria) : base(kwota, data)
+        //public WydatekRaz(decimal kwota, DateTime data, KategoriaWydatku kategoria) : base(kwota, data)
+        //{
+        //    this.kategoria = kategoria;
+        //}
+        //public KategoriaWydatku Kategoria { get => kategoria; set => kategoria = value; }
+        public WydatekRaz(decimal kwota, DateTime data) : base(kwota, data)
+        {
+            Kategoria = "Inne";
+        }
+        public WydatekRaz(decimal kwota, DateTime data, string kategoria) : base(kwota, data)
         {
             this.kategoria = kategoria;
         }
-        public KategoriaWydatku Kategoria { get => kategoria; set => kategoria = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
     }
 }
