21ed512f0d1f096e0b515da138f19b8ad34b39b6
dmazurczak@student.agh.edu.pl <dmazurczak@student.agh.edu.pl>
2024-01-19 14:39:01 +0100

    19.01.2024

--- Aplikacja_do_zarzadzania_wydatkami/gui/obj/Debug/net6.0-windows/gui.AssemblyInfo.cs
+++ Aplikacja_do_zarzadzania_wydatkami/gui/obj/Debug/net6.0-windows/gui.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("gui")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7551fc88eb422a74bd64612e7b33c20e5b6cf78")]
 [assembly: System.Reflection.AssemblyProductAttribute("gui")]
