e01f60129d5e14f0c13eec7a6ecbfabdf1a1e79f
dmazurczak@student.agh.edu.pl <dmazurczak@student.agh.edu.pl>
2024-01-19 01:24:42 +0100

    sql

---
+++ Aplikacja_do_zarzadzania_wydatkami/Aplikacja_do_zarzadzania_wydatkami/Dane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Threading.Task;
+using System.IO;
+
+public class Dane
+{
+	public Dane()
+	{
+        // konfiguracja dzięki której możemy połączyć się z bazą danych
+        // Data Source=nazwa naszego serwera
+        // Integrated Security=True - łączymy się za pomocą konta domenowego a nie za pomocą login i hasło?
+        private string conString = "Data Source=ACERVERO\\SQLEXPRESS; Initial Catalog=DaneAplikacjaDoZarzadzaniaWydatkami, Integrated Security=True;";
+	
+        private void ModyfikacjaDanych(string zapytanie)
+        {
+        using (SqlConnection sCon = new SqlConnection(conString))
+        {
+
+        }
+        }
+    }
+}
